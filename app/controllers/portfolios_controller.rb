17a0cf9af0e7f59fd2bd1a08dbfbe68faa0be3ea
Jared Romney <jared.romney28@gmail.com>
2017-06-12 16:28:54 -0600

    Integrated nested attributes for portfolio model

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,6 +2,9 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+  def angular
+    @angular_portfolio = Angular.all
+  end
 
 	def new
 		@portfolio_item = Portfolio.new
