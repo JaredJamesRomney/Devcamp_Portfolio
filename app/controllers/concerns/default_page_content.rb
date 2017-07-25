9c30cbed238f987d4377574b77fa8b6466ce749a
Jared Romney <jared.romney28@gmail.com>
2017-07-25 09:40:25 -0600

    added favicon

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -7,7 +7,7 @@ module DefaultPageContent
 	end
 
   def set_page_defaults
-  	@page_title = "Devcamp Portfolio | My Portfolio Website"
+  	@page_title = "Jared Romney | My Portfolio Website"
   	@seo_keywords = "Jared Romney Porfolio"
   end
 end
